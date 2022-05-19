c3d12935be332dab1dddde24f5e358e2d0d4e7c2
Joshua Singer <joshuamlsinger@gmail.com>
2022-05-18 15:12:40 -1000

    Added Screenshots. Added login popup

--- src/main/java/com/example/eworldaccessrequest/security/SecurityConfig.java
+++ src/main/java/com/example/eworldaccessrequest/security/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.example.eworldaccessrequest.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -20,22 +25,49 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 //
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
 //        http
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(STATELESS);
+//                .formLogin().permitAll().and()
+//                .authorizeRequests()
+//                .antMatchers(HttpMethod.GET, "/api/v1/access/**").hasAnyAuthority("ROLE_ADMIN2")
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+
+
+        //GOOD ONE!
 //        http
-//                .authorizeRequests().anyRequest().permitAll();
+//                .formLogin()
+//                .permitAll()
+//                .and().csrf();
 //        http
-//                .addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-//
-//    }
+//                .authorizeRequests((authorizeRequests) ->
+//                        authorizeRequests
+//                                .antMatchers("/**").hasRole("ADMIN2")
+//                )
+//                .authorizeRequests()
+////                .antMatchers(GET, "/api/v1/access/employee/**").hasAnyAuthority("ROLE_ADMIN2")
+//                .anyRequest().authenticated()
+        ;
+//        http.authorizeRequests((requests) -> {
+//            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+//        });
+//        http.formLogin();
+//        http.httpBasic();
+    }
 
     @Bean
     @Override
